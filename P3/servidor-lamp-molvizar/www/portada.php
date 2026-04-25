92da9cdfd2ca64ff86fa1df71f1e7587df961e66
Leonin04 <davidbacasposadas@gmail.com>
2026-04-25 21:30:07 +0200

    Subido el lamp

---
+++ P3/servidor-lamp-molvizar/www/portada.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+require_once 'database.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader);
+
+try {
+
+    $query = "SELECT n.*, l.nombre_lugar, i.ruta_archivo as foto_portada 
+              FROM noticias n 
+              LEFT JOIN lugares l ON n.id_lugar = l.id 
+              LEFT JOIN imagenes i ON n.id = i.id_noticia AND i.portada = 1
+              ORDER BY n.fecha DESC";
+              
+    $stmt = $pdo->query($query);
+    $noticias = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo $twig->render('portada.twig', [
+        'noticias' => $noticias
+    ]);
+
+} catch (PDOException $e) {
+    echo "Error al cargar las noticias: " . $e->getMessage();
+}
